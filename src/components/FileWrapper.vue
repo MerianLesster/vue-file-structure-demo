cff820b5c1ec256ecf4c07a27060db80a38cadb5
Merian Lesster <merianlesster27.ml@gmail.com>
2025-12-07 18:34:02 +0100

    feat: add primeflex support and refactor file structure components

--- src/components/FileWrapper.vue
+++ src/components/FileWrapper.vue
@@ -1,21 +1,78 @@
 <template>
-  <template v-for="file in files" :key="file.id">
-    <File :file="file" v-model="toggleStore[file.id]">
-      <FileWrapper
-        v-if="file.nodes && file.nodes.length > 0"
-        :files="file.nodes"
-        v-model="toggleStore"
-      />
-    </File>
-  </template>
+  <div>
+    <div
+      :class="{
+        'file-wrapper': true,
+        selected: isItemSelected,
+      }"
+      @click="selectFile"
+    >
+      <i v-show="!isFile" :class="`pi ${isOpen ? 'pi-angle-down' : 'pi-angle-right'}`"></i>
+      <p>{{ computedIcon }}</p>
+      <p>{{ files.name }}</p>
+    </div>
+    <template v-if="isOpen">
+      <div class="nested-files" v-for="file in files.nodes" :key="file.id">
+        <FileWrapper :files="file" />
+      </div>
+    </template>
+  </div>
 </template>
 
 <script setup lang="ts">
-import File from '@/components/File.vue'
-import type { IContent, IFile } from '@/types/file'
+import type { IFile } from '@/types/file'
+import { computed, ref } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 
-const { files } = defineProps<{ files: IFile[] }>()
-const toggleStore = defineModel<IContent>({ required: true })
+const router = useRouter()
+const route = useRoute()
+const { files } = defineProps<{ files: IFile }>()
+const isOpen = ref(false)
+const isFile = computed(() => !files.nodes || files.nodes?.length === 0)
+const computedIcon = computed(() => {
+  if (isFile.value) {
+    return '📄'
+  } else {
+    return isOpen.value ? '📂' : '📁'
+  }
+})
+const isItemSelected = computed(() => route.query?.file === queryValue.value)
+const queryValue = computed(() => `${files.id}+${files.name.replace(' ', '')}`)
+
+const selectFile = () => {
+  isOpen.value = !isOpen.value
+  if (isFile.value) {
+    router.push({
+      query: {
+        file: queryValue.value,
+      },
+    })
+  }
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+p {
+  margin: 0px;
+}
+.file-wrapper {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
+  padding: 3px 10px;
+  border-radius: 5px;
+  user-select: none;
+}
+.file-wrapper.selected {
+  background-color: #7fcff4 !important;
+}
+.nested-files {
+  padding-left: 5px;
+  margin-left: 16px;
+  border-left: 1px solid #7fcff4;
+}
+.file-wrapper:hover {
+  background-color: #7fcff48a;
+}
+</style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sidebar">
-    <FileWrapper :files="files" v-model="toggleStore" />
+    <FileWrapper :files="files" />
   </div>
 </template>
 
@@ -9,29 +9,11 @@ import FileWrapper from '@/components/FileWrapper.vue'
 import { onMounted, ref } from 'vue'
 
 import { fileExplorerData } from '@/utils/data'
-import type { IFile, IContent } from '@/types/file'
+import type { IFile } from '@/types/file'
 
-const files = ref<IFile[]>([fileExplorerData])
+const files = ref<IFile>(fileExplorerData)
 
-const toggleStore = ref<IContent>({})
-
-onMounted(() => {
-  const iterate = (files: IFile[], obj: IContent) => {
-    files.forEach((file) => {
-      if (file.isFolder) {
-        obj[file.id] = {
-          name: file.name,
-          isOpen: false,
-        }
-      }
-      if (file.nodes && file.nodes.length > 0) {
-        iterate(file.nodes, obj)
-      }
-    })
-  }
-
-  iterate(files.value, toggleStore.value)
-})
+onMounted(() => {})
 </script>
 
 <style scoped></style>
